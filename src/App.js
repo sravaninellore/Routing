717f92b3b3ed390eb6ef33b3b93a268a2126d337
sravaninellore <85183913+sravaninellore@users.noreply.github.com>
2023-06-30 15:32:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// Published URL  ==>  https://samplerouting.ccbp.tech
-
 import {Route, Switch} from 'react-router-dom'
 import About from './components/About'
 import Contact from './components/Contact'
